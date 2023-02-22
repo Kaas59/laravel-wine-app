e503a672b4b8ff1a772a9f496028d795e5327b09
Kazuma Tomatsu <52665786+Kaas59@users.noreply.github.com>
2023-02-23 01:39:39 +0900

    fix

--- wine-app/resources/views/info.blade.php
+++ wine-app/resources/views/info.blade.php
@@ -4,9 +4,9 @@
             〇〇な時におすすめ
         </h2>
     </x-slot>
-    <div class="bg-slate-500 h-[79.5vh] p-3 w-screen">
-      <div class="flex mb-3">
-        <img src="/wine/00001.png" alt="" class="w-[40vw] mx-auto rounded-lg">
+    <div class="bg-slate-500 h-[79.5vh] py-3 w-screen text-white">
+      <div class="flex px-3">
+        <img src="/wine/00001.png" alt="" class="w-[40vw] mx-auto rounded-lg shadow-sm">
         <div class="my-3 flex flex-col justify-around px-3">
           <div>{{ $wine['color_id'] }} / {{ $wine['type_id'] }}</div>
           <div>{{ $wine['name'] }}</div>
@@ -17,7 +17,12 @@
           <div>{{ $wine['country_id'] }}産</div>
         </div>
       </div>
-      <div class="mb-3 mx-3 flex flex-col justify-around h-[15vh]">
+      <div class="w-full my-1">
+        <div class="flex flex-row-reverse px-3">
+          <button type="button" class="addListBtn bg-sky-600 hover:bg-sky-500 text-white rounded px-3 py-1 mx-1 shadow-sm">Add List</button>
+        </div>
+      </div>
+      <div class="mb-3 mx-3 px-3 flex flex-col justify-around h-[15vh]">
         <div class="flex">
           <div class="w-[30%] text-center">品種</div>
           <div class="w-[70%]">{{ $wine['breed_id'] }}</div>
@@ -32,10 +37,10 @@
         </div>
       </div>
       <div class="border-b-2 w-auto mx-2"></div>
-      <div class="w-full h-[30vh] border-r-green-100 text-white my-2 px-1">
-        <div class="text-black">他にもこんなワインはいかがですか</div>
+      <div class="w-full h-[31vh] border-r-green-100 text-white my-2">
+        <div class="text-black px-3">他にもこんなワインはいかがですか</div>
         <div class="flex flex-nowrap overflow-x-scroll mt-2">
-          <div class="flex w-[calc(100% * {{ count($list) }})] h-[25vh] text-black">
+          <div class="flex w-[calc(100% * {{ count($list) }})] h-[31vh] text-black">
             @include('components.list', $list)
           </div>
         </div>
--- wine-app/resources/views/mypage.blade.php
+++ wine-app/resources/views/mypage.blade.php
@@ -3,30 +3,31 @@
       <image src="https://pbs.twimg.com/profile_images/1191321800757542912/CQ1BigB4_400x400.jpg" class="rounded-full w-[35vw] h-[35vw] shadow-lg">
       <div class="text-3xl bold">{{ $user['name'] }}</div>
     </div>
-    <div>
-      <!-- <div class="h-[59.5vh] bg-slate-500 flex flex-col overflow-y-scroll">
-          <div class="bg-white m-2 h-full rounded text-center">リスト機能　　Comming Soon</div>
-          <div class="bg-white m-2 h-full rounded text-center">リスト機能　　Comming Soon</div>
-          <div class="bg-white m-2 h-full rounded text-center">リスト機能　　Comming Soon</div>
-          <div class="bg-white m-2 h-full rounded text-center">リスト機能　　Comming Soon</div>
+    <div class="bg-slate-500">
+      <div class="w-[100vw] h-auto text-center py-2 bg-slate-500/50 bg-gradient-to-t relative">
+          <span class="text-2xl font-bold">マイリスト</span>
+          <a href="/" class="absolute right-2 bg-blue-600 hover:bg-blue-500 text-white rounded px-3 py-1 shadow-sm">New</a>
       </div>
-    </div> -->
-    <!-- <div class="h-[59.5vh] w-full bg-slate-500 flex justify-center items-center"> -->
-      <!-- <div class="text-white">リスト機能 Coming Soon</div> -->
-    <!-- </div> -->
-    <div class="flex flex-col flex-nowrap overflow-y-scroll h-[59.5vh] w-[100vw] bg-slate-500">
-      <div>
-        @foreach($lists as $list)
-            <div class="h-[30vh] my-2">
-              <p class="m-2 ">{{ $list[0] }}<span class="mx-2">編集</span></p>
-              <div class="flex flex-nowrap overflow-x-scroll">
-                <div class="flex w-[calc(100% * {{ count($lists) }})] h-[28vh]">
-                  @include('components.list', ['list' => $list[1]])
+      <div class="flex flex-col flex-nowrap overflow-y-scroll h-[54vh] w-[100vw] bg-slate-500">
+        <div>
+          @foreach($lists as $list)
+              <div class="h-[32vh] my-2">
+                <div class="relative px-2 pt-2">
+                  <span class="text-xl font-medium">{{ $list[0] }}</span>
+                  <a href="/" class="absolute right-2 bg-sky-600 hover:bg-sky-500 text-white rounded px-3 py-1 shadow-sm">Edit</a>
+                </div>
+                <div class="flex flex-nowrap overflow-x-scroll">
+                  @php
+                    $width = count($lists) ;
+                  @endphp
+                  <div class="flex w-[calc(100% * {{ $width }})] h-[28vh]">
+                    @include('components.list', ['list' => $list[1]])
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="border-b-2 w-auto mx-2"></div>
-        @endforeach
+              <div class="border-b-2 w-auto mx-2"></div>
+          @endforeach
+        </div>
       </div>
     </div>
 </x-app-layout>
