e89672fdfb664350992befda4d41ec3975d0068c
Kazuma Tomatsu <52665786+Kaas59@users.noreply.github.com>
2023-02-23 23:17:48 +0900

    add view

--- wine-app/resources/views/search.blade.php
+++ wine-app/resources/views/search.blade.php
@@ -9,7 +9,6 @@
         </span>
         <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search for wines..." type="text" name="search"/>
       </label> -->
-      @env('local')
       <form>
         <div class="flex">
           <label for="search-dropdown" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search Category</label>
@@ -40,7 +39,6 @@
           </div>
         </div>
       </form>
-      @endenv
     </h2>
   </x-slot>
 
